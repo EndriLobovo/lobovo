428906ab3ba4a92ae88cbb6737c22b1e995dbd2d
EndriLobovo <36638453+EndriLobovo@users.noreply.github.com>
2018-02-20 17:49:00 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,14 +1,14 @@
 jQuery('form .message').on('keypress', function(e) {
 	var message = jQuery(".message").val();
         $.ajax({
-        	type: 'POST',
-			url: admin_ajax,
+        type: 'POST',
+	url: admin_ajax,
         data: {
-				action: 'keywordajax_submission',
-				'message': message,
-			}, 
-          success: function(data){
-          		jQuery('.form-message').html(data);
+		action: 'keywordajax_submission',
+		'message': message,
+	}, 
+        success: function(data){
+          	jQuery('.form-message').html(data);
             
           }
         });
