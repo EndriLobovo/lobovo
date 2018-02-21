04f4b6060eb44c9cb715352b611c09b91ec40b3e
EndriLobovo <36638453+EndriLobovo@users.noreply.github.com>
2018-02-21 17:39:58 +0100

    Update tools.php

--- tools.php
+++ tools.php
@@ -10,8 +10,9 @@ global $post;
 oxy_page_header( $post->ID, array( 'heading_type' => 'page' ) ); ?>
 
 
-<div class="container" style="margin-top: 50px;">
+<div class="container tools">
   <div class="col-md-8">
+    <h3>Check Keyword Density</h3>
       <form id="form" action="" method="post">
         <span class="col-md-12 keywords-textarea">
           <textarea placeholder="Please enter your text here"  class ="message" type="text" name="message" ></textarea>
@@ -21,7 +22,9 @@ oxy_page_header( $post->ID, array( 'heading_type' => 'page' ) ); ?>
   </div>
   <div class="col-md-4">
     <div class="keywords-density">
+       <div class="density-details">
       
+      </div> 
     </div>
 
   </div>
