aea9aeb0474404f91c48ded66f2d62c3c0931a87
EndriLobovo <36638453+EndriLobovo@users.noreply.github.com>
2018-02-21 15:36:30 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
-add_action('wp_ajax_nopriv_keywordajax_submission','keyword_ajax_handler');
-function keyword_ajax_handler(){
+add_action('wp_ajax_nopriv_keyword_ajax_density','keyword_ajax_density');
+add_action('wp_ajax_priv_keyword_ajax_density','keyword_ajax_density');
+function keyword_ajax_density(){
 
 $user_text = $_POST['message'];
 $words = str_word_count(strtolower($user_text),1);
@@ -8,11 +9,34 @@ $numWords = count($words);
 // array_count_values() returns an array using the values of the input array as keys and their frequency in input as values.
 $word_count = (array_count_values($words));
 arsort($word_count);
+if ($word_count != 0) {
+  $i=0;
   foreach ($word_count as $key=>$val) {
-    if ( $val > 3) { ?>
-            <?php echo "$key = $val times  Density: ".number_format(($val/$numWords)*100)."%<br/>\n"; ?> 
-      <?php }
+
+    if ( $val >= 0 && $i<=9) { 
+      ?>
+            <p><?php $percent = number_format(($val/$numWords)*100);  echo "$key = $val times  Density: ".number_format(($val/$numWords)*100)."%<br/>\n"; 
+            
+            if ($percent > 0.5 && $percent <=2.5) { ?>
+                <h6 class="keyword-succesfully-density" value="<?php echo $percent;?>"> <div id="myProgress">
+                  <?php $new_density = $percent*40; ?>
+                  <div id="myBar" style="width:<?php echo $new_density;?>%;"></div>
+                  </div>  
+                </h6>
+            <?php } 
+
+            else { ?>
+            <h6 class="keyword-failed-density" value="<?php echo $percent;?>"> <div id="myProgress">
+                <div id="myBar" style="width:<?php echo $percent;?>%;""> 
+                </div>
+              </div>  
+            </h6>
+              <?php }
+            ?> </p>
+      <?php } 
     }
+}
+  wp_die(); 
 }
 
 ?>
